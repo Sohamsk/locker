bd0a5a6dea408bff25ec623f793a23f0508937e9
Soham Kudalkar <sohamskudalkar02@gmail.com>
2025-08-10 00:02:47 +0530

    Implemented ext_session_lock protocol requirements and the session locks just fine but now i have to figure out how to unlock properly

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
+#include "ext-session-lock-v1-protocol.h"
 #include "shared_memory.h"
-#include "xdg-shell-client-protocol.h"
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -9,45 +10,150 @@
 #include <unistd.h>
 #include <wayland-client-protocol.h>
 #include <wayland-client.h>
+#include <xkbcommon/xkbcommon-keysyms.h>
+#include <xkbcommon/xkbcommon.h>
 
 struct prog_state {
 	struct wl_display *display;
 	struct wl_compositor *compositor;
 	struct wl_shm *shm;
 	struct wl_shm_pool *pool;
-	struct wl_surface *surface;
 	struct wl_buffer *buffer;
-	struct xdg_wm_base *xdg_base;
-	struct xdg_surface *xdg_surface;
-	struct xdg_toplevel *xdg_toplevel;
-	uint8_t closed;
+	struct wl_seat *seat;
+	struct wl_keyboard *keyboard;
+	struct wl_output *output;
+	//  NOTE: there can be multiple outputs each output is analogus to a
+	//  screen. On my current machine i only have one output and also for
+	//  simplicity i will only bind to one output
+
+	struct xkb_context *xkb_context;
+	struct xkb_keymap *xkb_keymap;
+	struct xkb_state *xkb_state;
+
+	struct ext_session_lock_manager_v1 *lock_manager;
+	struct ext_session_lock_v1 *session_lock;
+	struct wl_surface *surface;
+	struct ext_session_lock_surface_v1 *lock_surface;
+	uint8_t locked;
 };
 
-static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base,
-			     uint32_t serial) {
-	xdg_wm_base_pong(xdg_wm_base, serial);
+static void wl_keyboard_listener_keymap(void *data,
+					struct wl_keyboard *wl_keyboard,
+					uint32_t format, int32_t fd,
+					uint32_t size) {
+	struct prog_state *client_state = data;
+	assert(format == WL_KEYBOARD_KEYMAP_FORMAT_XKB_V1);
+
+	char *map_shm = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+	assert(map_shm != MAP_FAILED);
+
+	struct xkb_keymap *xkb_keymap = xkb_keymap_new_from_string(
+	    client_state->xkb_context, map_shm, XKB_KEYMAP_FORMAT_TEXT_V1,
+	    XKB_KEYMAP_COMPILE_NO_FLAGS);
+	munmap(map_shm, size);
+	close(fd);
+
+	struct xkb_state *xkb_state = xkb_state_new(xkb_keymap);
+	xkb_keymap_unref(client_state->xkb_keymap);
+	xkb_state_unref(client_state->xkb_state);
+	client_state->xkb_keymap = xkb_keymap;
+	client_state->xkb_state = xkb_state;
 }
 
-static const struct xdg_wm_base_listener xdg_wm_base_listener = {
-    .ping = xdg_wm_base_ping,
+static void wl_keyboard_listener_enter(void *data,
+				       struct wl_keyboard *wl_keyboard,
+				       uint32_t serial,
+				       struct wl_surface *surface,
+				       struct wl_array *keys) {
+	struct prog_state *client_state = data;
+	fprintf(stderr, "keyboard enter; keys pressed are:\n");
+	uint32_t *key;
+	wl_array_for_each(key, keys) {
+		char buf[128];
+		xkb_keysym_t sym = xkb_state_key_get_one_sym(
+		    client_state->xkb_state, *key + 8);
+		xkb_keysym_get_name(sym, buf, sizeof(buf));
+		fprintf(stderr, "sym: %-12s (%d), ", buf, sym);
+		xkb_state_key_get_utf8(client_state->xkb_state, *key + 8, buf,
+				       sizeof(buf));
+		fprintf(stderr, "utf8: '%s'\n", buf);
+	}
+}
+
+static void wl_keyboard_listener_key(void *data,
+				     struct wl_keyboard *wl_keyboard,
+				     uint32_t serial, uint32_t time,
+				     uint32_t key, uint32_t state) {
+	struct prog_state *client_state = data;
+	uint32_t keycode = key + 8;
+	xkb_keysym_t sym =
+	    xkb_state_key_get_one_sym(client_state->xkb_state, keycode);
+
+	if (state == WL_KEYBOARD_KEY_STATE_PRESSED && sym == XKB_KEY_Escape) {
+		if (client_state->session_lock) {
+			ext_session_lock_v1_destroy(client_state->session_lock);
+			client_state->session_lock = NULL;
+			client_state->locked = 0;
+		}
+	}
+}
+
+static void wl_keyboard_listener_leave(void *data,
+				       struct wl_keyboard *wl_keyboard,
+				       uint32_t serial,
+				       struct wl_surface *surface) {
+	fprintf(stderr, "keyboard leave\n");
+}
+
+static void
+wl_keyboard_listener_modifiers(void *data, struct wl_keyboard *wl_keyboard,
+			       uint32_t serial, uint32_t mods_depressed,
+			       uint32_t mods_latched, uint32_t mods_locked,
+			       uint32_t group) {
+	struct prog_state *client_state = data;
+	xkb_state_update_mask(client_state->xkb_state, mods_depressed,
+			      mods_latched, mods_locked, 0, 0, group);
+}
+
+static void wl_keyboard_listener_repeat_info(void *data,
+					     struct wl_keyboard *wl_keyboard,
+					     int32_t rate, int32_t delay) {
+	/* NOTE:Left as an exercise for the reader */
+}
+
+struct wl_keyboard_listener wl_keyboard_listener = {
+    .keymap = wl_keyboard_listener_keymap,
+    .enter = wl_keyboard_listener_enter,
+    .leave = wl_keyboard_listener_leave,
+    .key = wl_keyboard_listener_key,
+    .modifiers = wl_keyboard_listener_modifiers,
+    .repeat_info = wl_keyboard_listener_repeat_info,
 };
 
-void xdg_toplevel_configure(void *data, struct xdg_toplevel *xdg_toplevel,
-			    int32_t width, int32_t height,
-			    struct wl_array *states) {
-	// idk what i should do here
-}
-
-void xdg_toplevel_close(void *data, struct xdg_toplevel *xdg_toplevel) {
-
+void wl_seat_listener_capabilities(void *data, struct wl_seat *wl_seat,
+				   uint32_t capabilities) {
 	struct prog_state *state = data;
-	state->closed = 1;
+	//  TODO: make a keyboard
+	if ((capabilities & WL_SEAT_CAPABILITY_KEYBOARD) &&
+	    state->keyboard == NULL) {
+		state->keyboard = wl_seat_get_keyboard(state->seat);
+		wl_keyboard_add_listener(state->keyboard, &wl_keyboard_listener,
+					 state);
+	} else if (!(capabilities & WL_SEAT_CAPABILITY_KEYBOARD) &&
+		   state->keyboard != NULL) {
+		wl_keyboard_release(state->keyboard);
+		state->keyboard = NULL;
+	}
 }
 
-static const struct xdg_toplevel_listener xdg_toplevel_listener = {
-    .configure = xdg_toplevel_configure,
-    .close = xdg_toplevel_close,
-};
+void wl_seat_listener_name(void *data, struct wl_seat *wl_seat,
+			   const char *name) {
+	//  NOTE: intentionally left blank
+}
+
+static struct wl_seat_listener wl_seat_listener = {
+    .capabilities = wl_seat_listener_capabilities,
+    .name = wl_seat_listener_name};
 
 static void reg_handle_global(void *data, struct wl_registry *wl_registry,
 			      uint32_t name, const char *interface,
@@ -60,32 +166,28 @@ static void reg_handle_global(void *data, struct wl_registry *wl_registry,
 	} else if (strcmp(interface, wl_shm_interface.name) == 0) {
 		state->shm =
 		    wl_registry_bind(wl_registry, name, &wl_shm_interface, 1);
-	} else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
-		state->xdg_base = wl_registry_bind(wl_registry, name,
-						   &xdg_wm_base_interface, 1);
-		xdg_wm_base_add_listener(state->xdg_base, &xdg_wm_base_listener,
-					 state);
+	} else if (strcmp(interface,
+			  ext_session_lock_manager_v1_interface.name) == 0) {
+		state->lock_manager =
+		    wl_registry_bind(wl_registry, name,
+				     &ext_session_lock_manager_v1_interface, 1);
+	} else if (strcmp(interface, wl_seat_interface.name) == 0) {
+		state->seat =
+		    wl_registry_bind(wl_registry, name, &wl_seat_interface, 7);
+		wl_seat_add_listener(state->seat, &wl_seat_listener, state);
+	} else if (strcmp(interface, wl_output_interface.name) == 0) {
+		state->output = wl_registry_bind(wl_registry, name,
+						 &wl_output_interface, 4);
 	}
 	// printf("Interface: %s,\n version: %d,\n name: %d\n", interface,
 	// version, name);
 }
 
-static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface,
-				  uint32_t serial) {
-	struct prog_state *state = data;
-	xdg_surface_ack_configure(xdg_surface, serial);
-
-	wl_surface_attach(state->surface, state->buffer, 0, 0);
-	wl_surface_commit(state->surface);
-}
-
-struct xdg_surface_listener xdg_surface_listener = {
-    .configure = xdg_surface_configure,
-};
-
 static void reg_handle_global_remove(void *data,
 				     struct wl_registry *wl_registry,
-				     uint32_t name) {}
+				     uint32_t name) {
+	/* no operation */
+}
 
 static const struct wl_registry_listener reg_listener = {
     .global = reg_handle_global, .global_remove = reg_handle_global_remove};
@@ -102,7 +204,7 @@ void getDisplay(struct prog_state *state) {
 			"Failed to get registry from wayland display!!\n");
 		exit(EXIT_FAILURE);
 	}
-	fprintf(stdout, "Connection established!!\n");
+	// fprintf(stdout, "Connection established!!\n");
 	wl_registry_add_listener(registry, &reg_listener, state);
 	wl_display_roundtrip(state->display);
 	wl_registry_destroy(registry);
@@ -136,35 +238,88 @@ void createBuffer(uint32_t width, uint32_t height, uint32_t stride,
 
 	close(fd);
 	munmap(pool_data, shm_pool_size);
-	wl_shm_pool_destroy(state->pool);
-	wl_shm_destroy(state->shm);
 }
+
+void lock_locked(void *data, struct ext_session_lock_v1 *ext_session_lock_v1) {
+	struct prog_state *state = data;
+	state->locked = 1;
+}
+void lock_finished(void *data,
+		   struct ext_session_lock_v1 *ext_session_lock_v1) {
+	fprintf(stderr, "failed to lock session\n");
+	exit(EXIT_FAILURE);
+}
+
+struct ext_session_lock_v1_listener lock_listener = {
+    .locked = lock_locked,
+    .finished = lock_finished,
+};
+void lock_surface_configure(
+    void *data, struct ext_session_lock_surface_v1 *ext_session_lock_surface_v1,
+    uint32_t serial, uint32_t width, uint32_t height) {
+	fprintf(stderr, "Lock surface Configure called: %dx%d\n", width,
+		height);
+	struct prog_state *state = data;
+
+	uint32_t stride = width * 4;
+
+	if (!state->buffer) {
+		createBuffer(width, height, stride, state);
+		if (state->buffer) {
+			fprintf(stderr, "Buffer created successfully\n");
+		} else {
+			fprintf(stderr, "Buffer creation failed!\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	wl_surface_attach(state->surface, state->buffer, 0, 0);
+	ext_session_lock_surface_v1_ack_configure(ext_session_lock_surface_v1,
+						  serial);
+	wl_surface_commit(state->surface);
+}
+
+struct ext_session_lock_surface_v1_listener lock_surface_listener = {
+    .configure = lock_surface_configure,
+};
 
 int main() {
 	struct prog_state state = {0};
+	state.xkb_context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
 
 	getDisplay(&state);
 
-	uint32_t width = 1920, height = 1080;
-	uint32_t stride = width * 4;
+	state.session_lock =
+	    ext_session_lock_manager_v1_lock(state.lock_manager);
+	ext_session_lock_v1_add_listener(state.session_lock, &lock_listener,
+					 &state);
 
-	createBuffer(width, height, stride, &state);
+	wl_display_roundtrip(state.display);
 
 	state.surface = wl_compositor_create_surface(state.compositor);
-	state.xdg_surface =
-	    xdg_wm_base_get_xdg_surface(state.xdg_base, state.surface);
-	xdg_surface_add_listener(state.xdg_surface, &xdg_surface_listener,
-				 &state);
-	state.xdg_toplevel = xdg_surface_get_toplevel(state.xdg_surface);
-	xdg_toplevel_set_title(state.xdg_toplevel, "Example client");
-	xdg_toplevel_add_listener(state.xdg_toplevel, &xdg_toplevel_listener,
-				  &state);
-	wl_surface_commit(state.surface);
+	if (!state.surface) {
+		fprintf(stderr, "surface is null");
+		exit(2);
+	}
+	state.lock_surface = ext_session_lock_v1_get_lock_surface(
+	    state.session_lock, state.surface, state.output);
+	ext_session_lock_surface_v1_add_listener(
+	    state.lock_surface, &lock_surface_listener, &state);
+	// wl_surface_commit(state.surface);
 
-	while (!state.closed && wl_display_dispatch(state.display)) {
-		/* This space deliberately left blank */
+	wl_display_roundtrip(state.display);
+
+	fprintf(stderr, "locked: %d", state.locked);
+	while (state.locked) {
+		if (wl_display_dispatch(state.display) < 0) {
+			fprintf(stderr, "wl_display_dispatch() failed");
+			ext_session_lock_v1_destroy(state.session_lock);
+			break;
+		}
 	}
 
+	wl_shm_pool_destroy(state.pool);
+	wl_shm_destroy(state.shm);
 	wl_buffer_destroy(state.buffer);
 	wl_surface_destroy(state.surface);
 	wl_compositor_destroy(state.compositor);
